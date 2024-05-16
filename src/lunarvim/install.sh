200fddc25e21f1a99238a1cdbe708641a9a16f8f
Cadu Ribeiro <mail@cadu.dev>
2024-05-16 17:11:55 -0300

    Install lunarvim to /usr/local

--- src/lunarvim/install.sh
+++ src/lunarvim/install.sh
@@ -3,4 +3,5 @@ set -e
 
 echo "Activating feature 'lunarvim'"
 
+export INSTALL_PREFIX=/usr/local
 curl -sL "https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh" | bash -s -- --no-install-dependencies -y 1>/dev/null;
